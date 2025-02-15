99e422ac4b891f0968101411204e410db8aa9e96
dsddr02 <fldz5555a@outlook.com>
2025-02-16 00:14:28 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -103,7 +103,7 @@ def send_telegram_message(content):
 def main():
     # 获取最新优选IP
     ip_addresses_str = get_cf_speed_test_ip()
-    ip_addresses = ip_addresses_str.split(',')[0]
+    ip_addresses = ip_addresses_str.split(',')
     dns_records = get_dns_records(CF_DNS_NAME)
     push_telegram_content = []
     # 遍历 IP 地址列表
